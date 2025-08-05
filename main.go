acb3f463694d0796eef003424964e9ec76240c32
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2025-08-05 16:27:27 +0300

    Added creation dowload-dir in main.go

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/CALLlA-74/zip-26-07-25/config"
 	"github.com/CALLlA-74/zip-26-07-25/internal/controllers"
 	"github.com/CALLlA-74/zip-26-07-25/internal/repository"
 	"github.com/CALLlA-74/zip-26-07-25/internal/usecases"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -16,6 +18,13 @@ func main() {
 	fr := repository.NewFileRepo()
 	auc := usecases.NewArchiverUC(tr, fr)
 	router := controllers.NewRouter(auc)
+
+	if _, err := os.Stat(config.DownloadPath); os.IsNotExist(err) {
+		err := os.Mkdir(config.DownloadPath, 0700)
+		if err != nil {
+			logrus.Fatal("Mkdir [%s] error:", config.DownloadPath, err.Error())
+		}
+	}
 
 	router.Router.GET("/manage/health", func(context *gin.Context) {
 		context.Status(http.StatusOK)
